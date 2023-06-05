d6a0a1279f4b7646ecc6c379c3bae49794c7d025
omartins-zs <gabrielmatheus209@gmail.com>
2023-06-05 15:13:18 -0300

    Add Effect blur to header no JS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,46 @@
-/*=============== SHOW MENU ===============*/
+/*==================== SHOWING & HIDING MENU ====================*/
 const navMenu = document.getElementById("nav-menu"),
   navToggle = document.getElementById("nav-toggle"),
   navClose = document.getElementById("nav-close");
 
-/*===== MENU SHOW =====*/
+/*===== SHOW MENU =====*/
+/* Checking if constant exists */
 if (navToggle) {
   navToggle.addEventListener("click", () => {
     navMenu.classList.add("show-menu");
   });
 }
 
-/*===== MENU HIDDEN =====*/
+/*===== HIDE MENU =====*/
+/* Checking if constant exists */
 if (navClose) {
   navClose.addEventListener("click", () => {
     navMenu.classList.remove("show-menu");
   });
 }
-/*=============== REMOVE MENU MOBILE ===============*/
+/*==================== REMOVING MOBILE MENU ====================*/
 const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () => {
+function linkAction() {
   const navMenu = document.getElementById("nav-menu");
   // When we click on each nav__link, we remove the show-menu class
   navMenu.classList.remove("show-menu");
-};
+}
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== ADD BLUR TO HEADER ===============*/
+function blurHeader() {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
+  this.scrollY >= 50 ? header.classList.add("blur-header")
+                         : header.classList.remove("blur-header");
+}
+window.addEventListener("scroll", blurHeader);
+
+/*=============== EMAIL JS ===============*/
+
+/*=============== SHOW SCROLL UP ===============*/
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
